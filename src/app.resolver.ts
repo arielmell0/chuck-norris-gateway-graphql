e9a9df11b70f8f1438183de9c0575ff4edd23bb0
arielmell0 <arielmello@rede.ulbra.br>
2024-06-23 20:31:56 -0300

    feat: add get categories get fact by category and search jokes

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,12 +1,27 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { AppService } from './app.service';
 
 @Resolver()
 export class AppResolver {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Query(() => String)
   async getRandomFact() {
     return this.appService.getRandomFact();
+  }
+
+  @Query(() => [String])
+  async getCategories() {
+    return this.appService.getCategories();
+  }
+
+  @Query(() => String)
+  async getRandomFactByCategory(@Args('category') category: string) {
+    return this.appService.getRandomFactByCategory(category);
+  }
+
+  @Query(() => [String])
+  async searchJokes(@Args('query') query: string) {
+    return this.appService.searchJokes(query);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -13,4 +13,25 @@ export class AppService {
     );
     return response.data.value;
   }
+
+  async getCategories(): Promise<string[]> {
+    const response: AxiosResponse<any> = await firstValueFrom(
+      this.httpService.get('https://api.chucknorris.io/jokes/categories'),
+    );
+    return response.data;
+  }
+
+  async getRandomFactByCategory(category: string): Promise<string> {
+    const response: AxiosResponse<any> = await firstValueFrom(
+      this.httpService.get(`https://api.chucknorris.io/jokes/random?category=${category}`),
+    );
+    return response.data.value;
+  }
+
+  async searchJokes(query: string): Promise<string[]> {
+    const response: AxiosResponse<any> = await firstValueFrom(
+      this.httpService.get(`https://api.chucknorris.io/jokes/search?query=${query}`),
+    );
+    return response.data.result.map((joke: any) => joke.value);
+  }
 }
